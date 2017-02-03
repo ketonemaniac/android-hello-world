6efba6a374638bbf774e133632625bee9e99726e
ketonemaniac <ketonemaniac@gmail.com>
2017-02-04 00:26:15 +0800

    basic configuration changes to run in Android Studio with a working Layout Designer

---
+++ app/src/main/java/com/sqisland/android/hello/DummyActivity.java
@@ -0,0 +1,14 @@
+package com.sqisland.android.hello;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class DummyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy);
+    }
+
+}
