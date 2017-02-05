45815c4d8fa7a5aab24697505a0bf08237d51eee
ketonemaniac <ketonemaniac@gmail.com>
2017-02-06 00:01:38 +0800

    button interaction

--- app/src/main/java/com/sqisland/android/hello/MainActivity.java
+++ app/src/main/java/com/sqisland/android/hello/MainActivity.java
@@ -2,11 +2,21 @@ package com.sqisland.android.hello;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+    }
+
+    public void onShowButtonClicked(View view) {
+        TextView helloText = (TextView) findViewById(R.id.helloText);
+        helloText.setVisibility(View.VISIBLE);
+    }
+
 }
