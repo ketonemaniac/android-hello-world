3358723ce1b2bef593f99db0da132bd562a9ae54
ketonemaniac <ketonemaniac@gmail.com>
2017-02-10 23:15:30 +0800

    "Now Loading" dialog, non blocking UI thread

--- app/src/main/java/com/sqisland/android/hello/MainActivity.java
+++ app/src/main/java/com/sqisland/android/hello/MainActivity.java
@@ -1,8 +1,10 @@
 package com.sqisland.android.hello;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,28 +18,33 @@ import java.util.concurrent.TimeoutException;
 
 public class MainActivity extends Activity {
 
+    Handler handler;
+    ProgressDialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        handler = new Handler();
+        dialog = ProgressDialog.show(this, "Loading", "Loading News...");
+
         // Cannot do internet connections on main thread!! Sprawn one thread to do it.
         RetrieveFeedTask task = new RetrieveFeedTask();
-        try {
-            task.execute();
-            IotdHandler handler = task.get(10, TimeUnit.SECONDS);
-            resetDisplay(handler.getTitle(), handler.getDate(), handler.getImage(), handler.getDescription());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-        }
+        task.execute(handler, this);
+        // can't do this since this will BLOCK the UI thread (and no repaint will result)
+        // This is why "handler" is used -- pass it to AsyncTask
+        // for UI update calls to be put in UI thread.
+        // IotdHandler handler = task.get(10, TimeUnit.SECONDS);
     }
 
     public void onShowButtonClicked(View view) {
         TextView helloText = (TextView) findViewById(R.id.helloText);
         helloText.setVisibility(View.VISIBLE);
+    }
+
+    public void postDisplay(IotdHandler iotdHandler) {
+        resetDisplay(iotdHandler.getTitle(), iotdHandler.getDate(), iotdHandler.getImage(), iotdHandler.getDescription());
+        dialog.dismiss();
     }
 
     private void resetDisplay(String title, String date, Bitmap image, String description) {
--- app/src/main/java/com/sqisland/android/hello/io/RetrieveFeedTask.java
+++ app/src/main/java/com/sqisland/android/hello/io/RetrieveFeedTask.java
@@ -1,16 +1,30 @@
 package com.sqisland.android.hello.io;
 
 import android.os.AsyncTask;
+import android.os.Handler;
+
+import com.sqisland.android.hello.MainActivity;
 
 /**
  * Async task to retrieve feed.
  */
-public class RetrieveFeedTask extends AsyncTask<String, Void, IotdHandler> {
+public class RetrieveFeedTask extends AsyncTask<Object, Void, IotdHandler> {
 
     @Override
-    protected IotdHandler doInBackground(String ... urls) {
-        IotdHandler handler = new IotdHandler();
+    protected IotdHandler doInBackground(final Object ... params) {
+        final IotdHandler handler = new IotdHandler();
         handler.processFeed();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ((Handler)params[0]).post(new Runnable() {
+            @Override
+            public void run() {
+                ((MainActivity)params[1]).postDisplay(handler);
+            }
+        });
         return handler;
     }
 
